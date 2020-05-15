917c6a959b9e68782ccf75a27a1780dcaea0a8f1
Nick Robinson <nicholas.robinson@invenialabs.co.uk>
2020-05-15 17:12:56 +0100

    Use nested testsets

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,43 +1,56 @@
-using Documenter
+using Documenter: doctest
 using IndexedDims
 using Test
 
 @testset "IndexedDims.jl" begin
     doctest(IndexedDims)
 
-    data = [1 2 3; 4 5 6; 7 8 9]
-    a = IndexedDimsArray(data, 2:4, 2:4)
+    @testset "constructor" begin
+        @test ==(
+            IndexedDimsArray([1 2; 3 4; 5 6]),
+            IndexedDimsArray([1 2; 3 4; 5 6], Base.OneTo(2), Base.OneTo(3))
+        )
+    end
 
-    # constructor
-    @test ==(
-        IndexedDimsArray([1 2; 3 4; 5 6]),
-        IndexedDimsArray([1 2; 3 4; 5 6], Base.OneTo(2), Base.OneTo(3))
-    )
+    @testset "standard fallback indexing" begin
+        data = [1 2 3; 4 5 6; 7 8 9]
+        a = IndexedDimsArray(data, 2:4, 2:4)
 
-    # standard fallback
-    @test a[1] == data[1]
-    @test a[3, 2] == data[3, 2]
-    @test a == data
-    @test a[:, :] == data[:, :]
-    @test a[:, :] isa IndexedDimsArray
-    @test a[:] == data[:]
+        @test a[1] == data[1]
+        @test a[3, 2] == data[3, 2]
+        @test a == data
+        @test a[:, :] == data[:, :]
+        @test a[:, :] isa IndexedDimsArray
+        @test a[:] == data[:]
+    end
 
-    # fancy
-    @test a[:, :] == a
-    @test typeof(a[:, :]) == typeof(a)
-    @test a[3:4, 2:3] == data[2:3, 1:2]
-    @test a[3:4, 2:3] isa IndexedDimsArray
-    @test a[3:4, :] == data[2:3, :]
-    @test a[3:4, bypass(1:2)] == data[2:3, 1:2]
+    @testset "fancy indexing" begin
+        @testset "UnitRange" begin
+            data = [1 2 3; 4 5 6; 7 8 9]
+            a = IndexedDimsArray(data, 2:4, 2:4)
 
-    # strings
-    a = IndexedDimsArray([1 2 3; 4 5 6; 7 8 9], ["a", "b", "c"], ["b", "a", "b"])
-    @test a[1] == data[1]
-    @test a[3, 2] == data[3, 2]
-    @test a[:, :] == a
-    @test typeof(a[:, :]) == typeof(a)
-    @test a[["b", "c"], "a"] == data[[2, 3], 2]
-    @test a[["b", "c"], "b"] in (data[[2, 3], 1], data[[2, 3], 3])
-    @test a[["b", "c"], ["b"]] == data[[2, 3], [1, 3]]
-    @test a["b", ["b", "a"]] == data[2, 1:3]
+            @test a[:, :] == a
+            @test typeof(a[:, :]) == typeof(a)
+
+            @test a[3:4, 2:3] isa IndexedDimsArray
+            @test a[3:4, 2:3] == data[2:3, 1:2]
+            @test a[3:4, :] == data[2:3, :]
+            @test a[3:4, bypass(1:2)] == data[2:3, 1:2]
+        end
+
+        @testset "Strings" begin
+            data = [1 2 3; 4 5 6; 7 8 9]
+            a = IndexedDimsArray(data, ["a", "b", "c"], ["b", "a", "b"])
+
+            @test a[:, :] == a
+            @test typeof(a[:, :]) == typeof(a)
+
+            @test a[1] == data[1]
+            @test a[3, 2] == data[3, 2]
+            @test a[["b", "c"], "a"] == data[[2, 3], 2]
+            @test a[["b", "c"], "b"] in (data[[2, 3], 1], data[[2, 3], 3])
+            @test a[["b", "c"], ["b"]] == data[[2, 3], [1, 3]]
+            @test a["b", ["b", "a"]] == data[2, 1:3]
+        end
+    end
 end
