cc3cbec4492f31bc173a503505e482af60712ec7
Nick Robinson <nicholas.robinson@invenialabs.co.uk>
2020-05-15 16:38:15 +0100

    Try to explain `src/base.jl`

--- src/base.jl
+++ src/base.jl
@@ -1,6 +1,25 @@
-# this file contains code copied from Base
-# multidimensional.jl, 289
-@generated function _unsafe_getindex_old!(dest::AbstractArray, src::AbstractArray, I::Vararg{Union{Real, AbstractArray}, N}) where N
+# Defining our own `_unsafe_getindex!` means we don't have to rely on Base internals.
+# If there is a performance cost, we may want to switch to a copy of the Base version,
+# but we would then need to call `to_indices` earlier to avoid passing in `Colon`.
+function _unsafe_getindex!(
+    dest::AbstractArray,
+    src::AbstractArray,
+    I::Vararg{Union{Real, AbstractArray, Colon}, N}
+) where N  # `where N` used to force specialisation
+    @inbounds for (i, j) in zip(eachindex(dest), Iterators.product(to_indices(src, I)...))
+        dest[i] = src[j...]
+    end
+    return dest
+end
+
+#=
+Optimized version (from Base) of the `_unsafe_getindex!` function above.
+Can use this if it improves performance, but ideally we'd avoid an `@generated` function.
+
+# This code is copied from Base/multidimensional.jl
+# https://github.com/JuliaLang/julia/blob/f12cde0e8c7f4268838d3201dd9e13b90ea44633/base/multidimensional.jl#L761-L777
+# Does not support `Colon`, as it assumes `I == to_indices(src, I)`.
+@generated function _unsafe_getindex!(dest::AbstractArray, src::AbstractArray, I::Vararg{Union{Real, AbstractArray}, N}) where N
     quote
         Base.@_inline_meta
         D = eachindex(dest)
@@ -16,10 +35,4 @@
         return dest
     end
 end
-
-function _unsafe_getindex!(dest::AbstractArray, src::AbstractArray, I::Vararg{Union{Real, AbstractArray, Colon}, N}) where N
-    @inbounds for (i, j) in zip(eachindex(dest), Iterators.product(Base.to_indices(src, I)...))
-        dest[i] = src[j...]
-    end
-    return dest
-end
+=#
