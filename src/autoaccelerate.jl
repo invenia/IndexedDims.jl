2e523ada4b6a8f5c6f1262c9e396ffb531c5ed8b
Nick Robinson <npr251@gmail.com>
2020-05-21 16:13:02 +0100

    Document `reindex`

--- src/autoaccelerate.jl
+++ src/autoaccelerate.jl
@@ -46,20 +46,47 @@ function autoaccelerate(v::AbstractVector)
     end
 end
 
-for index_type in (UniqueSortIndex, SortIndex, UniqueHashIndex)
-    @eval _reindex_type(::Type{<:$index_type}, index::AbstractUnitRange) = $index_type
-end
 
-_reindex_type(::Type{<:Union{HashIndex, UniqueHashIndex}}, index) = HashIndex
+"""
+    reindex(vec::AcceleratedVector, inds) -> AcceleratedVector
 
+Return an `AcceleratedVector` from a subset of the given `vec`.
+
+The returned `AcceleratedVector` may have the same underlying data.
+
+The returned `AcceleratedVector` is not guarnteed to have the same `AbstractIndex` type as
+the given `vec`, and the `AbstractIndex` index type of the return is not part of the API.
+
+```jldoctest; setup = :(using AcceleratedArrays, IndexedDims)
+julia> v = accelerate(4:8, UniqueSortIndex)
+5-element UnitRange{Int64} + UniqueSortIndex:
+ 4
+ 5
+ 6
+ 7
+ 8
+
+julia> IndexedDims.reindex(v, [3, 2, 1])
+3-element Array{Int64,1} + HashIndex (3 unique elements):
+ 6
+ 5
+ 4
+```
+"""
+reindex(v::AcceleratedVector, ::Colon) = v
 
 function reindex(v::AcceleratedVector{<:Any, <:Any, I}, index) where I
     return accelerate(parent(v)[index], _reindex_type(I, index))
 end
 
-reindex(v::AcceleratedVector, ::Colon) = v
-
 # no way to know whether a given AbstractArray is unique or sorted
 function reindex(v::AcceleratedVector{<:Any, <:Any, I}, index::AbstractArray) where I
     return accelerate(parent(v)[index], HashIndex)
 end
+
+# Ranges should be unique and sorted, so we should not lose any unique/sorted properties.
+for index_type in (UniqueSortIndex, SortIndex, UniqueHashIndex)
+    @eval _reindex_type(::Type{<:$index_type}, index::AbstractRange) = $index_type
+end
+
+_reindex_type(::Type{<:Union{HashIndex, UniqueHashIndex}}, index) = HashIndex
