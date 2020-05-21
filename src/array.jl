bfefa25910ce2df6d3e23ba53fb06496ac6a1e28
Nick Robinson <npr251@gmail.com>
2020-05-21 16:20:59 +0100

    Rename all the internal things

--- src/array.jl
+++ src/array.jl
@@ -1,16 +1,16 @@
 struct IndexedDimsArray{T, N, A<:AbstractArray{T, N}, Is<:Tuple{Vararg{<:AcceleratedVector, N}}} <: AbstractArray{T, N}
     data::A
-    indexes::Is
+    indices::Is
 end
 
 IndexedDimsArray(data::AbstractArray) = IndexedDimsArray(data, axes(data)...)
 
-function IndexedDimsArray(data::AbstractArray{T, N}, dim_inds::Vararg{AcceleratedVector, N}) where {T, N}
-    return IndexedDimsArray(data, dim_inds)
+function IndexedDimsArray(data::AbstractArray{T, N}, inds::Vararg{AcceleratedVector, N}) where {T, N}
+    return IndexedDimsArray(data, inds)
 end
 
-function IndexedDimsArray(data::AbstractArray{T, N}, dim_inds::Vararg{AbstractVector, N}) where {T, N}
-    return IndexedDimsArray(data, map(autoaccelerate, dim_inds))
+function IndexedDimsArray(data::AbstractArray{T, N}, inds::Vararg{AbstractVector, N}) where {T, N}
+    return IndexedDimsArray(data, map(autoaccelerate, inds))
 end
 
 Base.parent(arr::IndexedDimsArray) = arr.data
@@ -20,14 +20,5 @@ function Base.similar(
     arr::IndexedDimsArray{T, N, A},
     inds::Tuple{Vararg{Union{Int, AbstractUnitRange}, N}}
 ) where {T, N, A}
-    return IndexedDimsArray(similar(parent(arr), inds), map(reindex, arr.indexes, inds))
-end
-
-### simple fallback indexing (might delete later)
-function Base.getindex(arr::IndexedDimsArray{T, N}, inds::Vararg{Int, N}) where {T, N}
-    return Base.getindex(parent(arr), inds...)
-end
-
-function Base.setindex!(arr::IndexedDimsArray{T, N}, val, inds::Vararg{Int, N}) where {T, N}
-    return Base.setindex!(parent(arr), val, inds...)
+    return IndexedDimsArray(similar(parent(arr), inds), map(reindex, arr.indices, inds))
 end
--- src/dimindexing.jl
+++ src/dimindexing.jl
@@ -1,63 +1,99 @@
 struct BypassIndex{T}
-    index::T
+    val::T
 end
 
 bypass(val::T) where {T} = BypassIndex{T}(val)
 
 """
-    dim_to_indices(arr::IndexedDimsArray{T, N}, inds::Tuple{Vararg{Any, N}}) where {T, N}
+    parent_indices(arr::IndexedDimsArray{T, N}, inds::Tuple{Vararg{Any, N}}) where {T, N}
 
-Convert indexes into the IndexedDimsArray into indexes into the underlying array.
+Return indicies of the underlying array corresponding the indices `inds` of the
+`IndexedDimsArray` `arr`.
 
 ```jldoctest; setup = :(using IndexedDims)
 julia> arr = IndexedDimsArray([4.0, 5.0], ["a", "b"]);
 
-julia> IndexedDims.dim_to_indices(arr, (["b"],))
+julia> IndexedDims.parent_indices(arr, (["b"],))
 ([2],)
 ```
 """
-function dim_to_indices(arr::IndexedDimsArray{T, N}, inds::Tuple{Vararg{Any, N}}) where {T, N}
-    return map(unwrap_index, arr.indexes, inds)
+function parent_indices(arr::IndexedDimsArray{T, N}, inds::Tuple{Vararg{Any, N}}) where {T, N}
+    return map(parent_index, arr.indices, inds)
 end
 
-function unwrap_index(acc::AcceleratedVector{T, <:AbstractVector{T}, <:AcceleratedArrays.AbstractUniqueIndex}, ind) where T
-    return _unwrap_unique_index(acc, ind)
-end
-_unwrap_unique_index(acc::AcceleratedVector, ind) = findfirst(isequal(ind), acc)
-_unwrap_unique_index(acc::AcceleratedVector, ind::Union{AbstractArray, Colon, BypassIndex}) = _unwrap_index(acc, ind)
-unwrap_index(acc::AcceleratedVector, ind) = _unwrap_index(acc, ind)
+"""
+    parent_index(v::AcceleratedVector, idx)
+
+Return the index of `idx` in the underlying vector `parent(v)`p
+
+```jldoctest; setup = :(using IndexedDims: autoaccelerate, _parent_index)
+julia> v = autoaccelerate(4:6);
+
+julia> idx = 5:6;
+
+julia> parent_idx = _parent_index(v, idx)
+2:3
+
+julia> @assert idx == parent(v)[parent_idx]
+```
+"""
+parent_index(acc::AcceleratedVector, idx) = _parent_index(acc, idx)
 # NOTE: we don't guarantee that we will find the first match, just any single match
-_unwrap_index(acc::AcceleratedVector, ind) = findfirst(isequal(ind), acc)
-_unwrap_index(acc::AcceleratedVector, ind::AbstractArray) = findall(in(ind), acc)
-_unwrap_index(acc::AcceleratedVector, ::Colon) = Colon()
-_unwrap_index(acc::AcceleratedVector, ind::BypassIndex) = ind.index
+_parent_index(acc::AcceleratedVector, idx) = findfirst(isequal(idx), acc)
+_parent_index(acc::AcceleratedVector, idx::AbstractArray) = findall(in(idx), acc)
+_parent_index(acc::AcceleratedVector, ::Colon) = Colon()
+_parent_index(acc::AcceleratedVector, idx::BypassIndex) = idx.val
 
 # --- #
 # TODO: move to AcceleratedArrays.jl
-
-function _unwrap_index(acc::AcceleratedVector{T, <:AbstractUnitRange{T}}, ind::AbstractRange{<:Integer}) where T<:Integer
-    return findall(in(ind), parent(acc))
+function _parent_index(
+    acc::AcceleratedVector{T, <:AbstractUnitRange{T}},
+    idx::AbstractRange{<:Integer}
+) where T<:Integer
+    return findall(in(idx), parent(acc))
 end
-
 # --- #
 
-function dim_getindex(arr::IndexedDimsArray{T, N}, inds::Vararg{Any, N}) where {T, N}
-    unwrapped_indices = dim_to_indices(arr, inds)
-    new_indices = (reindex(arr_idx, idx) for (arr_idx, idx) in zip(arr.indexes, unwrapped_indices) if !(idx isa Integer))
-    new_underlying_indices = (Base.OneTo(length(ind)) for ind in new_indices)
-    new_array = IndexedDimsArray(similar(parent(arr), new_underlying_indices...), new_indices...)
-    _unsafe_getindex!(parent(new_array), parent(arr), unwrapped_indices...)
-    return new_array
+#=
+# treat `<:AbstractUniqueIndex`s differently
+function parent_index(acc::AcceleratedVector{T, <:AbstractVector{T}, <:AbstractUniqueIndex}, ind) where T
+    return _parent_unique_index(acc, ind)
 end
 
-function dim_setindex!(arr::IndexedDimsArray{T, N}, val, inds::Vararg{Any, N}) where {T, N}
-    return Base.setindex!(parent(arr), val, dim_to_indices(arr, inds)...)
+_parent_unique_index(acc::AcceleratedVector, ind) = findfirst(isequal(ind), acc)
+function _parent_unique_index(acc::AcceleratedVector, ind::Union{AbstractArray, Colon, BypassIndex})
+    return _parent_index(acc, ind)
 end
+=#
 
+# Fancy getindex
 function Base.getindex(arr::IndexedDimsArray{T, N}, inds::Vararg{Any, N}) where {T, N}
-    return dim_getindex(arr, inds...)
+    par = parent(arr)
+    par_inds = parent_indices(arr, inds)
+
+    inds
+    new_arr_inds = (reindex(a, p) for (a, p) in zip(arr.indices, par_inds) if !(p isa Integer))
+    new_par_inds = (Base.OneTo(length(ind)) for ind in new_arr_inds)
+
+    # new_arr = IndexedDimsArray(similar(parent(arr), new_par_inds...), new_arr_inds...)
+    new_par = similar(par, new_par_inds...)
+    # _unsafe_getindex!(parent(new_arr), parent(arr), par_inds...)
+    _unsafe_getindex!(new_par, par, par_inds...)
+    # return new_arr
+    return IndexedDimsArray(new_par, new_arr_inds...)
 end
 
+# Int getindex
+function Base.getindex(arr::IndexedDimsArray{T, N}, inds::Vararg{Int, N}) where {T, N}
+    return Base.getindex(parent(arr), inds...)
+end
+
+# Fancy setindex!
 function Base.setindex!(arr::IndexedDimsArray{T, N}, val, inds::Vararg{Any, N}) where {T, N}
-    return dim_setindex!(arr, inds...)
+    return Base.setindex!(parent(arr), val, parent_indices(arr, inds)...)
+end
+
+# Int setindex!
+function Base.setindex!(arr::IndexedDimsArray{T, N}, val, inds::Vararg{Int, N}) where {T, N}
+    return Base.setindex!(parent(arr), val, inds...)
 end
